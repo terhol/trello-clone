080a5d3ddfaae3bdaaee71b456cbd61d3969e4e1
Tereza Holm <holm.tereza@gmail.com>
2024-03-07 10:21:17 +0100

    Added basic components

---
+++ src/Card.tsx
@@ -0,0 +1,9 @@
+import { CardContainer } from "./styles"
+
+type CardProps = {
+    text:String
+}
+
+export const Card = ({text}: CardProps) => {
+    return <CardContainer>text</CardContainer>
+}
---
+++ src/Column.tsx
@@ -0,0 +1,17 @@
+import { ColumnContainer, ColumnTitle } from "./styles";
+import { Card } from "./Card";
+
+type ColumnProps = {
+    text: String
+}
+
+ export const Column = ({ text }: ColumnProps) => {
+    return (
+        <ColumnContainer>
+        <ColumnTitle>{text}</ColumnTitle>
+        <Card text="Generate app scaffold" />
+        <Card text="Learn TypeScript" />
+        <Card text=">Begin to use static typing" />
+        </ColumnContainer>
+    )
+}
