8637744af7e1e8bdc5e074c6881a3af01e122e1f
Tereza Holm <holm.tereza@gmail.com>
2024-03-21 17:10:22 +0100

    Added useFocus

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Column } from './Column';
 import { AppContainer } from './styles';
+import { AddNewItem } from './AddNewItem';
 
 export const App = () => {
   return (
     <AppContainer>
       <Column text="Todo:"/>
+      <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log}/>
     </AppContainer>
   )
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,5 +1,6 @@
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { Card } from "./Card";
+import { AddNewItem } from "./AddNewItem";
 
 type ColumnProps = {
     text: String
@@ -12,6 +13,8 @@ type ColumnProps = {
         <Card text="Generate app scaffold" />
         <Card text="Learn TypeScript" />
         <Card text="Begin to use static typing" />
+
+        <AddNewItem toggleButtonText="+Add another card" onAdd={console.log} dark/>
         </ColumnContainer>
     )
 }
